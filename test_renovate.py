57fc723d273d18245ffb1b2eec3ef9855d9deccf
Devin Buhl <devin.kray@gmail.com>
2021-02-16 08:39:56 -0500

    fix: workflow

--- test_renovate.py
+++ test_renovate.py
@@ -8,3 +8,6 @@ def test_renovate():
   result = runner.invoke(cli, ['--cluster-path', './tests/'])
   assert result.exit_code == 0
   assert result.output == 'Hello Peter!\n'
+
+if __name__ == '__main__':
+    test_renovate()
