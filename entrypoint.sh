3473e364c998ba816557059404a47e52567d4761
Allen Porter <allen@thebends.org>
2021-06-09 20:10:59 -0700

    Convert shell script to bash to address ShellCheck errors

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -l
+#!/bin/bash -l
 
 CLUSTER_PATH="${1}"
 EXCLUDE_FOLDERS="${2}"
@@ -6,22 +6,22 @@ DEBUG="${3}"
 DRY_RUN="${4}"
 TOLERATE_YAML_ERRORS="${5}"
 
-FLAGS="--cluster-path=\"${CLUSTER_PATH}\""
+FLAGS=(--cluster-path="${CLUSTER_PATH}")
 
 if [ -n "${EXCLUDE_FOLDERS}" ]; then
-  FLAGS="$FLAGS --excluded-folders=\"${EXCLUDE_FOLDERS}\""
+  FLAGS+=(--excluded-folders="${EXCLUDE_FOLDERS}")
 fi
 
 if [ "${DEBUG}" = "yes" ]; then
-    FLAGS="$FLAGS --debug"
+    FLAGS+=("--debug")
 fi
 
 if [ "${DRY_RUN}" = "yes" ]; then
-    FLAGS="$FLAGS --dry-run"
+    FLAGS+=("--dry-run")
 fi
 
 if [ "${TOLERATE_YAML_ERRORS}" = "yes" ]; then
-    FLAGS="$FLAGS --tolerate-yaml-errors"
+    FLAGS+=("--tolerate-yaml-errors")
 fi
 
-/usr/local/bin/python /app/renovate.py ${FLAGS}
+/usr/local/bin/python /app/renovate.py ${FLAGS[@]}
